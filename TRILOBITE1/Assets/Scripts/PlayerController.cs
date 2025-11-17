acea3b7c0b54ed4c29028ceb44347a3d92200cd1
Misfaris <constanzarigazio@hotmail.com>
2025-11-17 01:55:48 +0000

    TRILOBITE_002_FixInputPhysicsAndCollisions
    Fixed Vertical input controls to correctly move the Trilobite forward/backward along the Z axis.
    Added separate Horizontal input to control left/right movement.
    Froze the Rigidbody rotation in X, Y, and Z.
    Added Box Collider to Stromatolite (obstacle).
    Added Rigidbody-based movement.
    Replaced transform.Translate with trilobiteRb.AddForce so physics-based collisions work properly.
    Deactivated OceanFloor and gave FLOOR plane a Mesh collider and Sand_floor.001 Material.
    Locked player rotation using trilobiteRb.freezeRotation = true; to stop the Trilobite from tipping over.

--- TRILOBITE1/Assets/Scripts/PlayerController.cs
+++ TRILOBITE1/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    
+    public float speed = 30.0f;
+    public float verticalInput;
+    public float horizontalInput;
+    public float xRange = 36;
+    public float zRange = 100;
+    private Rigidbody trilobiteRb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        //On this GameObject (the Trilobite), find the Rigidbody component and store it inside rb
+        trilobiteRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //Move Trilobite forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        trilobiteRb.AddForce(Vector3.back * (speed * verticalInput));
+
+        //Move Trilobite side to side
+        trilobiteRb.AddForce(Vector3.left * (speed * horizontalInput));
+
+        //Keep Trilobite in bounds
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        if (transform.position.z < -zRange)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        if (transform.position.z > zRange)
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        
+        //Stop Trilobite from tipping over
+        trilobiteRb.freezeRotation = true;
     }
 }
